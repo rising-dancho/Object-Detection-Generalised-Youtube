ae5bfcb95e06b860e1613104ebc353f40c7bf0e2
rising-dancho <zenlaws02@gmail.com>
2025-04-09 17:21:47 +0800

    feat: trained a 50 epoch tensorflow lite model

--- config.py
+++ config.py
@@ -16,5 +16,5 @@ MODEL_PATH = f'{cwd}/model'
 MODEL = 'efficientdet_lite0'
 MODEL_NAME = 'hardware_supplies.tflite'
 CLASSES =  ['Bistay sand', 'Cement', 'Gravel', 'Hollow blocks', 'Rebar', 'Sack', 'Skim coat']
-EPOCHS = 20
+EPOCHS = 50
 BATCH_SIZE = 4
