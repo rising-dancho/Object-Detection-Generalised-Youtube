91cafd0d554ea1b681fe31e30bb52c173169e4fe
rising-dancho <zenlaws02@gmail.com>
2025-04-10 22:25:40 +0800

    feat: trying to figure out the output of the tensorflow lite model that was created

--- config.py
+++ config.py
@@ -16,5 +16,5 @@ MODEL_PATH = f'{cwd}/model'
 MODEL = 'efficientdet_lite0'
 MODEL_NAME = 'hardware_supplies.tflite'
 CLASSES =  ['Bistay sand', 'Cement', 'Gravel', 'Hollow blocks', 'Rebar', 'Sack', 'Skim coat']
-EPOCHS = 50
+EPOCHS = 20
 BATCH_SIZE = 4
--- train_model.py
+++ train_model.py
@@ -1,41 +1,54 @@
-from tflite_model_maker import object_detector
+import warnings
+warnings.filterwarnings("ignore")
 
-import config
+from tflite_model_maker import model_spec
+from tflite_model_maker import object_detector
 import tensorflow as tf
 import os
 
-# Load config values
+import config
+
 EPOCHS = config.EPOCHS
 BATCH_SIZE = config.BATCH_SIZE
 MODEL_NAME = config.MODEL_NAME
-	@@ -15,6 +18,7 @@
+CLASSES = config.CLASSES
+MODEL_PATH = config.MODEL_PATH
+TRAIN_DATASET_PATH = config.TRAIN_DATASET_PATH
 VALID_DATASET_PATH = config.VALID_DATASET_PATH
 MODEL = config.MODEL
 
-# Load datasets
 train_data = object_detector.DataLoader.from_pascal_voc(
     TRAIN_DATASET_PATH,
     TRAIN_DATASET_PATH,
-	@@ -27,8 +31,8 @@
     CLASSES
 )
 
-# Create model
+val_data = object_detector.DataLoader.from_pascal_voc(
+    VALID_DATASET_PATH,
+    VALID_DATASET_PATH,
+    CLASSES
+)
+
 spec = model_spec.get(MODEL)
+spec.config.max_instances_per_image = 200  # or higher than your largest image instance count
+
 model = object_detector.create(
     train_data,
     model_spec=spec,
-	@@ -38,10 +42,32 @@
+    batch_size=BATCH_SIZE,
+    train_whole_model=True,
+    epochs=EPOCHS,
     validation_data=val_data
 )
 
-# Evaluate and export
 model.evaluate(val_data)
+
 model.export(export_dir=MODEL_PATH, tflite_filename=MODEL_NAME)
 
 print('-'*100)
 print('Training completed.')
 print('See the model folder.')
+
 
 # ✅ Check exported TFLite model output tensor shapes
 print("\nAnalyzing output tensor shapes of exported model:")
